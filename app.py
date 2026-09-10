f4e925def91c0ef6172a021f1e30465633ec5ea7
alnasardinner-dotcom <alnasardinner@gmail.com>
2026-09-10 12:20:44 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Ensure current directory is in sys.path for Streamlit Cloud & Linux deployment
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import pandas as pd
 import json
